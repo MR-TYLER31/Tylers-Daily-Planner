aac8e90263ac84dbdc7f3a8fe15166949c451e58
Tyler Snyder <tyler@Tylers-MacBook-Pro.local>
2019-12-19 17:27:04 -0700

    create variables to hold current date, object for time values, forLoop to create new time row divs and elements within div

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,33 @@
-let m = moment()
+$(document).ready(function() {
 
-// console.log(`toString() => ${m.toString()}`);
+  // Create variables to hold the current date and hour
+  var containerDiv = $('.container');
+  var date = moment().format('dddd, MMMM Do YYYY')
+  $('#current-date').text(date);
+ var currentHour = parseInt(moment().format('H'))
+console.log(currenthour)
 
+// Create array of objects to hold the values for each our of the day
+var timeValues = [
+  {time: "9AM"},
+  {time: "10AM"},
+  {time: "11AM"},
+  {time: "12AM"},
+  {time: "1PM"},
+  {time: "2PM"},
+  {time: "3AM"},
+  {time: "4PM"},
+  {time: "5PM"}
+]
 
-var currentDate = $('#current-date');
-
-
-
-function date() {
-    currentDate.text(m.format("dddd, MMMM Do YYYY"));
+// create a for loop to that iterates through the time values array and make a row for each hour.
+for(var i = 0; i < timeValues.length; i++) {
+  var timeRow = $('<div>');
+  var hourDiv = $('<div>');
+  var textDiv = $('<div>');
+  var textAreaDiv = $('<textarea');
+  var buttonDiv = $('<div>');
+  var saveButton = $('<button>');
 }
 
-date()
+});
