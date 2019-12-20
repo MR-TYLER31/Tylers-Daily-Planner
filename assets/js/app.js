c802aa65b0a40b31aa14412900514e6bc88aba0b
Tyler Snyder <tyler@Tylers-MacBook-Pro.local>
2019-12-19 21:00:13 -0700

    made function to loop through the rows and set color according to the hour

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,16 +8,17 @@ $(document).ready(function() {
 console.log(currentHour)
 
 // Create array of objects to hold the values for each our of the day
+// milTime is the time in milatary time because moment.js displays military time
 var timeValues = [
-  {time: "9AM"},
-  {time: "10AM"},
-  {time: "11AM"},
-  {time: "12AM"},
-  {time: "1PM"},
-  {time: "2PM"},
-  {time: "3AM"},
-  {time: "4PM"},
-  {time: "5PM"}
+  {time: "9AM", milTime: 09},
+  {time: "10AM", milTime: 10},
+  {time: "11AM", milTime: 11},
+  {time: "12AM", milTime: 12},
+  {time: "1PM", milTime: 13},
+  {time: "2PM", milTime: 14},
+  {time: "3AM", milTime: 15},
+  {time: "4PM", milTime: 16},
+  {time: "5PM", milTime: 17}
 ]
 
 // create a for loop to that iterates through the time values array and dynamically make a row for each hour.
@@ -28,7 +29,7 @@ for(var i = 0; i < timeValues.length; i++) {
   // create div to hold the textarea content
   var textDiv = $('<div>');
   // create textarea element to type in task
-  var textAreaDiv = $('<textarea>');
+  var textArea = $('<textarea>');
    // create div to hold the button
   var buttonDiv = $('<div>');
   // create the button to save for local storage
@@ -36,10 +37,41 @@ for(var i = 0; i < timeValues.length; i++) {
 
   // add classes to the created elements
   timeRow.addClass('row');
-  hourDiv.addClass('col-md-1');
-  hourDiv.text(`<p>${timeValues[i].time}</p>`)
+  hourDiv.addClass('col-md-1'); 
+  hourDiv.html(`<p>${timeValues[i].time}</p>`) // Displays the hour
+  textDiv.addClass('col-md-10');
+  textDiv.attr('value', timeValues[i].milTime); // holds the time value in milary time because moment.js is military time
+  buttonDiv.addClass('col-md-1');
+  saveButton.addClass('saveBtn');
+  saveButton.html(`<i class="fas fa-lock"></i>`)
 
+  // append the row to the container div
+  containerDiv.append(timeRow);
+  // append the time, textareaDiv and button div to th row
+  timeRow.append(hourDiv, textDiv, buttonDiv);
+  // append the textarea element to the textareaDiv
+  textDiv.append(textArea);
+  // apppend the saveButton to the buttonDiv
+  buttonDiv.append(saveButton);
 }
+
+// Loop through the textarea rows and change the color according to the current hour of the day
+function colorDiv() {
+  // 
+  var rowColor = $(".col-md-10");
+  for (let j = 0; j < rowColor.length; j++) {
+    var currentDiv = $(rowColor[j]);
+    if (currentDiv.attr("value") > currentHour) {
+      currentDiv.addClass("future");
+    } else if (currentDiv.attr("value") < currentHour) {
+      currentDiv.addClass("past");
+    } else {
+      currentDiv.addClass("present");
+    }
+  }
+}
+// Call the colorDiv function
+colorDiv();
 
 
 
